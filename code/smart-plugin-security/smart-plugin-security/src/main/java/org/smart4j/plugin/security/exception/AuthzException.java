1cf85101af56291ac2240aebabc234b0bca7b440
libo_yu <libo_yu>
2017-07-04 13:32:29 +0800

    Signed-off-by: libo_yu <libo_yu>

---
+++ code/smart-plugin-security/smart-plugin-security/src/main/java/org/smart4j/plugin/security/exception/AuthzException.java
@@ -0,0 +1,25 @@
+package org.smart4j.plugin.security.exception;
+
+/**
+ * 授权异常（当权限无效时抛出）
+ *
+ * @since 1.0.0
+ */
+public class AuthzException extends RuntimeException {
+
+    public AuthzException() {
+        super();
+    }
+
+    public AuthzException(String message) {
+        super(message);
+    }
+
+    public AuthzException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public AuthzException(Throwable cause) {
+        super(cause);
+    }
+}
